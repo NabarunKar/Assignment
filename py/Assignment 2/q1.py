5714d545c5466bfa342d1b572cc3519584a69312
NabarunKar <71139016+NabarunKar@users.noreply.github.com>
2020-09-17 23:04:01 +0530

    Create q1.py

---
+++ py/Assignment 2/q1.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first integer:"))
+b = int(input("Enter second integer:"))
+
+if(a > b):
+    print(a, " is greater")
+elif(x < y):
+    print(b, " is greater")
+else:
+    print("They are equal")
