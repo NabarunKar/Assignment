89495a4a2e195c7184db86fe54fdb294e56c5042
NabarunKar <71139016+NabarunKar@users.noreply.github.com>
2021-01-13 22:28:43 +0530

    Create amphibians.py

---
+++ Modules and Packages/Packages/amphibians.py
@@ -0,0 +1,14 @@
+def examples():
+    print("Examples of Amphibians: ")
+    lst=['Frog' , 'Toads' , 'Newts' , 'Caecilians' , 'Salamander']
+    for i in lst:
+        print(i)
+    print('\n\n')    
+
+
+def chars():
+    ch=["Cold Blooded" ,"Lays eggs" , "Moist scaleless skin" , "Can live both on land and water" ]
+    print("Characteristics of Amphibians : ")
+    for i in ch:
+        print(i)        
+    print('\n\n')    
