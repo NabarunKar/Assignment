f5aa45938b4c2fa119ed8661d82ddb6e0204f7da
NabarunKar <71139016+NabarunKar@users.noreply.github.com>
2021-01-14 17:19:58 +0530

    Add files via upload

---
+++ Modules and Packages 2.0 (14th January)/Modules/q3.py
@@ -0,0 +1,4 @@
+import calendar
+print("Month names of calendar are: ")
+for name in calendar.month_name:
+    print(name)
---
+++ Modules and Packages 2.0 (14th January)/Modules/q4.py
@@ -0,0 +1,4 @@
+import calendar
+print("Days in a week are: ")
+for name in calendar.day_name:
+    print(name)
